a57f24e4e830d8c19f5695de9b58e53c048b2c0a
jstzwjr <wangjianrongtz@163.com>
2025-08-08 15:49:10 +0800

    first commit

---
+++ SampleApp/src/Utils/BuildId.hpp
@@ -0,0 +1,17 @@
+//==============================================================================
+//
+//  Copyright (c) 2020, 2024 Qualcomm Technologies, Inc.
+//  All Rights Reserved.
+//  Confidential and Proprietary - Qualcomm Technologies, Inc.
+//
+//==============================================================================
+
+#pragma once
+
+namespace qnn {
+namespace tools {
+
+inline std::string getBuildId() { return std::string("v2.29.0.241129103708_105762"); }
+
+}  // namespace tools
+}  // namespace qnn
